d7a28a3520ce712a88b7f61a3a876b3b9c08c30c
Daniel Kummer <daniel.kummer@gmail.com>
2011-08-31 09:55:32 +0200

    clean up logger class

--- lib/terrimporter/app_logger.rb
+++ lib/terrimporter/app_logger.rb
@@ -1,12 +1,20 @@
 class Logger
   attr_accessor :level
 
-  NUMBER_TO_NAME_MAP = {0=>'DEBUG', 1=>'INFO', 2=>'WARN', 3=>'ERROR', 4=>'FATAL', 5=>'UNKNOWN'}
-  NUMBER_TO_COLOR_MAP = {0=>'0;37', 1=>'32', 2=>'33', 3=>'31', 4=>'31', 5=>'37'}
-  #CUSTOM_FORMAT = "[%s] %5s: %s\n"
-  LOG_FORMAT = "\033[0;37m[%s]\033[0m[\033[%sm %5s \033[0m]: %s \n"
-  #"#{message =}" "\033[0;37m#{Time.now.to_s(:db)}\033[0m [\033[#{color}m" + sprintf("%-5s","#{sevstring}") + "\033[0m] #{message.strip} (pid:#{$$})\n" unless message[-1] == ?\n
-  TIME_FORMAT = "%H:% M:%S"
+  NUMBER_TO_NAME_MAP  = {0=>'DEBUG',  1=>'INFO',  2=>'WARN',  3=>'ERROR', 4=>'FATAL', 5=>'UNKNOWN'}
+  NUMBER_TO_COLOR_MAP = {0=>'0;37',   1=>'32',    2=>'33',    3=>'31',    4=>'31',    5=>'37'}
+
+  # more infos: https://wiki.archlinux.org/index.php/Color_Bash_Prompt
+  #\033[0m      Text reset
+  #\033[0;37m   White
+  #\033[032m    Green
+  #\033[033m    Yellow
+  #\033[031m    Red
+  #\033[037m    White
+
+  # %s => [datetime], %s => color, %-5s => severity, %s => message
+  LOG_FORMAT = "\033[0;37m %s \033[0m[\033[%sm%-5s\033[0m]: %s \n"
+  TIME_FORMAT = "%H:%M:%S"
 
   def initialize
     self.level = 0
@@ -24,8 +32,7 @@ class Logger
     return if self.level > severity
     sevstring = NUMBER_TO_NAME_MAP[severity]
     color = NUMBER_TO_COLOR_MAP[severity]
-    self.output (LOG_FORMAT % [format_datetime(Time.new),color, sevstring, message])
-    #"\033[0;37m#{Time.now.to_s(:db)}\033[0m [\033[#{color}m" + sprintf("%-5s","#{sevstring}") + "\033[0m] #{message.strip} (pid:#{$$})\n"
+    self.output (LOG_FORMAT % [format_datetime(Time.now),color, sevstring, message])
   end
 
   def format_datetime(time)
