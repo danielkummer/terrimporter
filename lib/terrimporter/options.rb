a5375e296b52c2a7de95befaf618384fb4b04018
Daniel Kummer <daniel.kummer@gmail.com>
2011-09-14 21:59:14 +0200

    clean up options

--- lib/terrimporter/options.rb
+++ lib/terrimporter/options.rb
@@ -5,9 +5,7 @@ module TerrImporter
 
       def initialize(args)
         super()
-
         @orig_args = args.clone
-
         self[:verbose] = false
         self[:show_help] = false
 
@@ -15,44 +13,24 @@ module TerrImporter
         @opts = OptionParser.new do |o|
           o.banner = "Usage: #{File.basename($0)} [options] \n" +
           "Use #{File.basename($0)} [application_url] --init to initialize importer before first usage."
-
           o.separator ''
           o.separator 'Common options:'
-
           o.on('-a', '--all', 'import everything configured; javascripts, css files and images') do
             self[:import_css] = true
             self[:import_js] = true
             self[:import_images] = true
             self[:import_modules] = true
           end
-
           o.on('-c', '--css', 'import configured css files') { self[:import_css] = true }
-
           o.on('-i', '--img', 'import configured image files') { self[:import_images] = true }
-
           o.on('-j', '--js', 'import configured javascript files') { self[:import_js] = true }
-
           o.on('-m', '--module', 'import configured module files') { self[:import_modules] = true }
-
-          o.on('--init [CONFIG_EXISTS]', [:backup, :replace], 'create configuration file in current working directory. use optional argument to force file replacement (backup, replace)') do |init|
-            self[:init] = init || true
-          end
-
-          o.on('-f', '--config CONFIG_FILE', 'use alternative configuration file') do |config_file|
-            self[:config_file] = config_file
-          end
-
+          o.on('--init [CONFIG_EXISTS]', [:backup, :replace], 'create configuration file in current working directory. use optional argument to force file replacement (backup, replace)') { |init| self[:init] = init || true }
+          o.on('-f', '--config CONFIG_FILE', 'use alternative configuration file') { |config_file| self[:config_file] = config_file }
           o.separator ''
           o.separator 'Additional configuration:'
-
-          o.on('-v', '--[no-]verbose', 'run verbosely') do |v|
-            self[:verbose] = v
-          end
-
-          o.on('--version', 'Show version') do
-            self[:show_version] = true
-          end
-
+          o.on('-v', '--[no-]verbose', 'run verbosely') { |v| self[:verbose] = v }
+          o.on('--version', 'Show version') { self[:show_version] = true }
           o.on_tail('-h', '--help', 'display this help and exit') { self[:show_help] = true }
         end
 
@@ -64,7 +42,6 @@ module TerrImporter
         rescue OptionParser::InvalidArgument => e
           self[:invalid_argument] = e.message
         end
-
       end
 
       def merge(other)
