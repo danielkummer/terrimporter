006399095cf7fc20eaa3bfdc81fe33359c4bbc98
Daniel Kummer <daniel.kummer@gmail.com>
2011-09-26 21:14:47 +0200

    css validation

--- lib/terrimporter/importer.rb
+++ lib/terrimporter/importer.rb
@@ -54,23 +54,28 @@ module TerrImporter
           constructed_file_path = (config.replace_style_strings? ? unclean_file_path : file_path)
           @downloader.download(source_url, constructed_file_path)
 
-          if config.replace_style_strings?
-            LOG.info "CSS line replacements"
-            File.open(file_path, 'w') do |d|
-              File.open(constructed_file_path, 'r') do |s|
-                lines = s.readlines
-                lines.each do |line|
-                  d.print replace_stylesheet_lines!(line)
+          if file_contains_valid_css?(file_path)
+            if config.replace_style_strings?
+              LOG.info "CSS line replacements"
+              File.open(file_path, 'w') do |d|
+                File.open(constructed_file_path, 'r') do |s|
+                  lines = s.readlines
+                  lines.each do |line|
+                    d.print replace_stylesheet_lines!(line)
+                  end
                 end
               end
+            else
+              LOG.debug "Skipping css line replacements"
+            end
+
+            if File.exists?(unclean_file_path)
+              LOG.debug "Deleting unclean css files"
+              FileUtils.remove unclean_file_path
             end
           else
-            LOG.debug "Skipping css line replacements"
-          end
-
-          if File.exists?(unclean_file_path)
-            LOG.debug "Deleting unclean css files"
-            #FileUtils.remove unclean_file_path
+            File.remove(file_path)
+            LOG.debug "Deleting empty"
           end
         end
       end
--- lib/terrimporter/importer_helper.rb
+++ lib/terrimporter/importer_helper.rb
@@ -6,4 +6,16 @@ module ImporterHelper
     line.gsub! what, with
   end
 
+  def file_contains_valid_css?(file_path)
+    File.open(file_path) do |f|
+      valid = false
+      f.each_line do |line|
+        if line.include?(pattern)
+
+        end
+      end
+    return true
+    end
+  end
+
 end
